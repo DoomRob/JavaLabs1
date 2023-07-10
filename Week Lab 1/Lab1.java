89e4b9930caa5a8c7627444cd1a4dc80a7fef5c3
Robert Gannon <123395116+DoomRob@users.noreply.github.com>
2023-07-10 11:28:12 +0100

    Add files via upload

---
+++ Week Lab 1/Lab1.java
@@ -0,0 +1,26 @@
+public class Lab1
+{
+	public static void main(String args[])
+	{
+
+	System.out.println("*");
+	System.out.println("**");
+	System.out.println("***");
+	System.out.println("**");
+	System.out.println("*");
+
+	System.out.println("RRRRRRR      GGGGGGGGG");
+	System.out.println("RR   RR      GGG    GG");
+	System.out.println("RRRRRRR      GGG");
+	System.out.println("RR  RRR      GGG   GGG");
+	System.out.println("RR   RRR     GGG    GG");
+	System.out.println("RRR   RRR    GGGGGGGGG");
+
+	/*
+	/Lab1
+	/Author Robert Gannon
+	/Date: 27/05/22
+	*/
+
+	}
+}
---
+++ Week Lab 1/Lab2.java
@@ -0,0 +1,13 @@
+/*
+*MyFirstProgram.java
+*@author Robert Gannon
+* Date: 27/05/22
+*/
+
+public class Lab2{
+	public static void main(String args[]){
+		System.out.println("I love Maths");
+		System.out.println("And I love java too");
+
+	}
+}
